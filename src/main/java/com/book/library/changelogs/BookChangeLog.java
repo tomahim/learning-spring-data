3f8ac3428413b7ee4834fa2213d3ee975bfb3999
tomahim <thimblot@gmail.com>
2017-10-15 21:38:49 +0200

    init mongo collections at each start

--- src/main/java/com/book/library/changelogs/BookChangeLog.java
+++ src/main/java/com/book/library/changelogs/BookChangeLog.java
@@ -1,19 +1,43 @@
 package com.book.library.changelogs;
 
+import com.book.library.domain.Book;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.mongobee.changeset.ChangeLog;
 import com.github.mongobee.changeset.ChangeSet;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
-import com.book.library.domain.Book;
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
 
 @ChangeLog
 @Component
 public class BookChangeLog {
 
-    @ChangeSet(order="001", id="test", author = "testAuthor")
-    public void someChange(MongoTemplate mongoTemplate) {
-        mongoTemplate.save(new Book("Toto"));
+    final boolean runAlways = true;
+
+    @ChangeSet(order="001", id="reinitBookCollection", author = "testAuthor", runAlways = runAlways)
+    public void reinitBookCollection(MongoTemplate mongoTemplate) {
+        mongoTemplate.dropCollection("books");
+        mongoTemplate.createCollection("books");
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            List<Book> books = mapper.readValue(getFile("books.json"), new TypeReference<List<Book>>() {});
+            mongoTemplate.insertAll(books);
+            Book book = new Book("Toto");
+            book.setYear(1880);
+            mongoTemplate.save(book);
+        } catch (Exception e) {
+
+        }
+    }
+
+    private File getFile(String fileName) {
+        return new File("/C:/Users/Thomas/dev/library/src/main/resources/books.json");
     }
 
 }
